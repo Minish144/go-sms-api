8c178bea519493a58ec2edc3700701f2350bd22d
Nikolai Varlamov <varlamow.col@yahoo.com>
2021-09-30 05:42:03 -0700

    Server initialization

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"github.com/minish144/go-sms-api/modules/server"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
 func main() {
-	
+	// viper init
+	viper.AddConfigPath("./")
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	err := viper.ReadInConfig()
+	if err != nil {
+		logrus.WithFields(logrus.Fields{
+			"error": err.Error(),
+		}).Fatalln()
+	}
+	logrus.Info("viper config initialized successfully")
+
+	// server init
+	server.Run()
 }
--- modules/server/server.go
+++ modules/server/server.go
@@ -1,2 +1,26 @@
 package server
 
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
+func Run() {
+	router := mux.NewRouter()
+
+	http.Handle("/", router)
+
+	connectionString := getListeningAddress()
+	logrus.Infof("Server is now listening on http://%s", connectionString)
+	http.ListenAndServe(connectionString, nil)
+}
+
+func getListeningAddress() string {
+	host := viper.GetString("server.interface")
+	port := viper.GetString("server.port")
+	return fmt.Sprintf("%s:%s", host, port)
+}
